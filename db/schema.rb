e146e1e080490882250a4edebb039506f61840eb
AyumiYamauti <ayumiyamauti@AyumiYamautinoMacBook-Pro.local>
2025-03-09 14:00:12 +0900

    add:ユーザー登録機能・ログイン機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_08_074156) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_09_004755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_08_074156) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
