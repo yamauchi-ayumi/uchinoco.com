caaadc2fdc5f197177c43d6577c9995b4b8867d3
AyumiYamauti <ayumiyamauti@AyumiYamautinoMacBook-Pro.local>
2025-03-12 13:39:53 +0900

    add:Postモデル・Postsテーブルを生成・編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_09_004755) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_12_023438) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +31,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_09_004755) do
     t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
