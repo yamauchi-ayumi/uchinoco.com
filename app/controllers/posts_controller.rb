bfe212ac9d745ee0fc01edfa7a6af70e7f977c12
AyumiYamauti <ayumiyamauti@AyumiYamautinoMacBook-Pro.local>
2025-03-14 15:07:12 +0900

    add:postコントローラーにnew・createアクションを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,22 @@ class PostsController < ApplicationController
   def index
     @posts = Post.includes(:user)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+      if @post.save
+        redirect_to posts_path, success: t('defaults.flash_message.created')
+      else
+        flash.now[:alert] = t('defaults.flash_message.not_created')
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  def post_params
+    params.require(:post).permit(:body, :post_image, :post_image_cache)
+  end
 end
